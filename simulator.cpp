95a996285d00d87ab2d49a5186b43fe32ea63e54
xiangnancode <xiangnan.g12@gmail.com>
2017-04-16 22:16:22 -0500

    trading simulator 1.0

--- simulator.cpp
+++ simulator.cpp
@@ -8,6 +8,12 @@
 
 using namespace std;
 
+struct Userinfo {
+	string name;
+	double fund;
+	unordered_map<string, int> accountdetial;
+};
+
 int writer(char *data, size_t size, size_t nmemb, string *buffer){
 	int result = 0;
 	if(buffer != NULL) {
@@ -76,7 +82,7 @@ string to_uppercase(string s) {
 
 void outputtradelog(string filename, string entry) {
   ofstream file;
-  file.open("./usersinfo/" + filename, std::ios_base::app);
+  file.open("./usersinfo/" + filename + "_trading_log", std::ios_base::app);
   if (!file.is_open()) {
   	cout << "file did not open.\n";
   }
@@ -84,10 +90,11 @@ void outputtradelog(string filename, string entry) {
   file.close();
 }
 
-void trade(string user, unordered_map<string, int> useraccountinfo) {
-	bool process = false;
+void trade(Userinfo &user) {
+	bool process = false; //trade command indicator
 	string buyorsell, symb;
 	int nshares;
+	char confirm;
 	while(!process) {
 		cout << "Input format:" << endl;
 		cout << "buy/sell symb #share(s)" << endl;
@@ -102,7 +109,6 @@ void trade(string user, unordered_map<string, int> useraccountinfo) {
 		}
 		cout << buyorsell << "ing " << nshares << " share(s) of " << symb << endl;
 		cout << "Please confirm: Y/N? ";
-		char confirm;
 		cin >> confirm;
 		if (confirm == 'Y') {
 			process = true;
@@ -112,18 +118,67 @@ void trade(string user, unordered_map<string, int> useraccountinfo) {
 		
 	}
 	cout << "Processing..." << endl;
-	float price = stod(getonlinedata(symb));
+	double price = stod(getonlinedata(symb));//get real time price
+	double totalprice = price * (double) nshares;
+	cout << "\n" << buyorsell << "ing " << nshares << " share(s) of " << (symb)  << " at $" << to_string(price) << ".\n\n";
+	// if buying
+	if (!buyorsell.compare("buy")) { 
+		if (user.fund < price * (double) nshares) {// if no enough fund to buy
+			cout << "Need " << to_string(totalprice) << " to buy, but you only have " << to_string(user.fund) << ".\n";
+			cout << "Enter Y to buy as much as you can (" << (int) (user.fund / price) << " shares), or enter N to terminate the trade.\n";
+			cin >> confirm;
+			if (confirm == 'Y') {
+				nshares = (int) (user.fund / price);
+				totalprice = price * (double) nshares;
+				cout << "\n" << buyorsell << "ing " << nshares << " share(s) of " << (symb)  << " at $" << to_string(price) << ".\n";
+			} else {
+				return;//terminate
+			}
+		}
+		if (user.accountdetial.find(symb) == user.accountdetial.end()) {
+			user.accountdetial[symb] = nshares;
+		} else {
+			user.accountdetial[symb] += nshares;
+		}
+		user.fund -= totalprice;
+	}
+	// if selling
+	if (!buyorsell.compare("sell")) { 
+		if (user.accountdetial.find(symb) == user.accountdetial.end()) {// check if holding
+			cout << "You are not holding " << symb << ".\nTrade terminated.";
+			return;//terminate
+		} else if (user.accountdetial[symb] < nshares) {// if no enough share to sell
+			cout << "You only hold " << user.accountdetial[symb] << " shares.\n";
+			cout << "Enter Y to sell them all, or enter N to terminate the trade.\n";
+			cin >> confirm;
+			if (confirm == 'Y') {
+				nshares = user.accountdetial[symb];
+				cout << "\n" << buyorsell << "ing " << nshares << " share(s) of " << (symb)  << " at $" << to_string(price) << ".\n";
+				totalprice = price * (double) nshares;
+			} else {
+				return;//terminate
+			}
+		}
+		if (nshares == user.accountdetial[symb]) {
+			user.accountdetial.erase(symb);
+		} else {
+			user.accountdetial[symb] -= nshares;
+		}
+		user.fund += totalprice;
+	}
+	//get current time
 	time_t now;
-		tm *gmtm;
-		now = time(0);
-		gmtm = gmtime(&now);
-	int side = buyorsell.compare("buy") ? -1 : 1;
+	tm *gmtm;
+	now = time(0);
+	gmtm = gmtime(&now);
 	string tradelog = to_string(gmtm->tm_mon + 1) + "-" + to_string(gmtm->tm_mday) + "-" + to_string(gmtm->tm_year + 1900) + ",";
 	tradelog += to_string(gmtm->tm_hour - 4) + ":" + to_string(gmtm->tm_min) + ":" + to_string(gmtm->tm_sec);
+	//
+	int side = buyorsell.compare("buy") ? -1 : 1;
 	tradelog += "," + (symb) + "," + to_string(side) + "," + to_string(nshares) + "," + to_string(price) + "\n";
-	cout << "\n" << buyorsell << "ing " << nshares << " share(s) of " << (symb)  << " at $" << price << ".\n\n";
+	//output trade log
 	cout << "\ntrade log: " << tradelog << endl;
-	outputtradelog(user, tradelog);
+	outputtradelog(user.name, tradelog);
 	//return tradelog;
 }
 
@@ -148,41 +203,85 @@ void creataccount(string username, string password, double fund) {
 	cout << "creating account...\n";
 	ofstream file;
   	file.open("./usersinfo/usersinfo", std::ios_base::app);
-  	file << "[\n" + username + "\n" + password + "\n" + to_string(fund) + "\n]\n";
-  	cout << "user " << username << "created\n";
+  	file << "[\n" + username + "\n" + password + "\n]\n";
   	file.close();
+  	file.open("./usersinfo/" + username + "_account_detial", std::ios_base::app);
+  	file << to_string(fund) + "\n";
+  	file.close();
+  	cout << "user " << username << " created\n";
+ }
+
+ void loaduserinfo(string username, Userinfo& user) {
+ 	cout << "loading user account detial...\n";
+ 	user.name = username;
+ 	ifstream file;
+ 	file.open("./usersinfo/" + username + "_account_detial");
+ 	if (!file.is_open()) {
+		cout << "file did not open.\n";
+	}
+ 	string entry, nshares;
+ 	getline(file, entry,'\n');
+ 	user.fund = stod(entry);
+ 	while (file.good()) {
+ 		getline(file, entry,',');
+ 		getline(file, nshares,'\n');
+ 		if (entry.size() != 0 && nshares.size() != 0) {
+ 			user.accountdetial[entry] = stoi(nshares);
+ 		}
+ 	}
+ 	file.close();
+ 	return;
+ }
+
+ void saveuserinfo(Userinfo& user) {
+	ofstream file;
+	file.open("./usersinfo/" + user.name + "_account_detial");
+	if (!file.is_open()) {
+		cout << "file did not open.\n";
+	}
+	file << to_string(user.fund) << "\n";
+	for (auto it = user.accountdetial.begin(); it != user.accountdetial.end(); ++it) {
+		file << it->first << "," << it->second << "\n";
+	}
+	file.close();
+ }
+
+ void accountstatus(Userinfo& user) {
+ 	cout << "\naccount status:\n" << "username: " << user.name  << "\nfund: " << to_string(user.fund) << "\nHolding:\n";
+ 	for (auto it = user.accountdetial.begin(); it != user.accountdetial.end(); ++it) {
+		cout << it->first << "," << it->second << "\n";
+	}
+	cout << endl;
  }
 
  void logintoaccount(string username) {
  	cout << "logging into account...\n";
  	ifstream file ("./usersinfo/usersinfo");
 	string entry, password;
-	double fund;
 	int count = 0;
 	while (file.good()) 
 	{
 		getline(file, entry,'[');
 		getline(file, entry,'\n');
 		getline(file, entry,'\n');
-		if (!username.compare(entry)) {
-			getline(file, entry,'\n');
-			while (1) {
+		if (!username.compare(entry)) { // find the username
+			getline(file, entry,'\n'); // get stored password
+			while (1) { // enter password
 				cout << "Please enter password: ";
 				cin >> password;
-				if (count == 3) return;
+				if (count == 3) return; // exit on the 3rd wrong password
 				if (password.compare(entry)) {
 					++count;
 					cout << "Password does not match...\n";
 				} else {
-					break;
+					break; // pass
 				}
 			}
-			getline(file, entry,'\n');
-			fund = stod(entry);
 		}
 	}
 	file.close();
-	unordered_map<string, int> useraccountinfo;
+	Userinfo user;
+	loaduserinfo(username, user); // load user info
 	while (1) {
 		cout << "menu:\nEnter 1 to check account status\nEnter 2 to trade\nEnter 0 to return to main menu\n";
 		cout << "Please enter: ";
@@ -190,16 +289,18 @@ void creataccount(string username, string password, double fund) {
 		cin >> key;
 		switch (key) {
 			case 1:
-			cout << "\naccount status:\n" << "username: " << username  << "\nfund: " << fund << "\n\n";
+			accountstatus(user);
 			break;
 			case 2:
-			trade(username, useraccountinfo);
+			trade(user);
 			break;
 			case 0:
+			//saveuserinfo(user);
 			return;
 			default:
 			cout << "Error input, please re-enter\n";
 		}
+		saveuserinfo(user);
 	}
 	
  }
@@ -207,10 +308,8 @@ void creataccount(string username, string password, double fund) {
 
 int main ()
 {
-	string log;
-	//log = trade();
-
 	while (1) {
+		// main menu
 		cout << "menu:\nEnter 1 to creat account\nEnter 2 to login to exsited account\nEnter 0 to exit this simulator\n";
 		cout << "Please enter: ";
 		int key;
@@ -218,20 +317,18 @@ int main ()
 		string username, password, password0;
 		double fund;
 		switch (key) {
-			case 1:
+			case 1:// create account
 			cout << "creating account...\n";
-			while (1) {
+			while (1) { //enter user name
 				cout << "Please enter username: ";
 				cin >> username;
-				if (checkusername(username)) {
-					cout << "username already exits, please try another one\n";
+				if (checkusername(username)) { // check if exist
+					cout << "username already exists, please try another one\n";
 				} else {
 					break;
 				}
 			}
-			
-
-			while (1) {
+			while (1) { // enter password
 				cout << "Please enter password: ";
 				cin >> password;
 				cout << "Please re-enter password: ";
@@ -242,11 +339,12 @@ int main ()
 					break;
 				}
 			}
+			//enter initial fund
 			cout << "Please enter fund: ";
 			cin >> fund;
-			creataccount(username, password, fund);
+			creataccount(username, password, fund);// setup new account
 			break;
-			case 2:
+			case 2: // login 
 			while (1) {
 				cout << "Please enter username: ";
 				cin >> username;
